e96551989647fd8c1e5ff2fc67d385c6b706d3c6
dev.benjamin.perez <dev.benjamin.perez@gmail.com>
2025-11-26 10:14:58 +0000

    file name changes

--- public/script.js
+++ public/script.js
@@ -395,6 +395,7 @@ function generateTimestamp12() {
   const HH = String(now.getHours()).padStart(2, "0");
   const mm = String(now.getMinutes()).padStart(2, "0");
   const ss = String(now.getSeconds()).padStart(2, "0");
+  console.log(yy + MM + dd + HH + mm + ss)
   return yy + MM + dd + HH + mm + ss;
 }
 
